569c3c9062e1366420ed5b97cf362705492348dc
zerquiolin <handres47@hotmail.com>
2022-09-09 22:07:30 -0500

    contexto

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World!</p>
+        <p>Infraestructura de TI 2022</p>
         <a
           className="App-link"
           href="https://reactjs.org"
